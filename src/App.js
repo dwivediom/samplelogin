734bc5531bbe8df62ac60635555aa6c30779b97d
dwivediom <dwivediom00@gmail.com>
2023-11-21 11:02:44 +0530

    add new

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import Login from './componets/Login/Login';
+import Register from './componets/register/Register';
 function App() {
 
   return (
     <div className="App">
        <h3>LOGIN </h3>
           <Login/>
-      
+       <h3>REGISTER</h3>
+       <Register/>
     </div>
   );
 }
--- src/componets/register/Register.jsx
+++ src/componets/register/Register.jsx
@@ -1,10 +1,14 @@
+
+
 import React, { useState } from 'react'
 import axios from "axios"
-import "./login.css"
-const Login = () => {
+import "./register.css"
+const Register = () => {
  const [email, setemail] = useState('')
  const [password, setpassword] = useState('')
  const [runlogin, setrunlogin] = useState(false)
+ const [name, setname] = useState('')
+ const [runerror, setrunerror] = useState(false)
  
 const [datamsg, setDatamsg] = useState('')
  const handleClick= async()=>{ 
@@ -12,13 +16,18 @@ const [datamsg, setDatamsg] = useState('')
      
       console.log("log")
 
-      const logindata = await axios.get(`http://localhost:5000/login?password=${password}&email=${email}`)
+      const logindata = await axios.post(`http://localhost:5000/register`, { email: email , password : password , name: name })
       console.log(logindata.data.msg)
 
       if(logindata.data.msg){
         setDatamsg(logindata.data.msg)
-      if(logindata.data.msg==="login successfull"){
+      if(logindata.data.msg==="user registered"){
         setrunlogin(true)
+        setrunerror(false)
+      }
+      if(logindata.data.msg==="user already exists, please login"){
+        setrunerror(true)
+        setrunlogin(false)
       }
     }
  }
@@ -26,10 +35,12 @@ const [datamsg, setDatamsg] = useState('')
     
     <div>
            <div className='contianer'>
+           <input className='input' type='text' placeholder='name' value={name} onChange={(e)=>setname(e.target.value)} /> 
            <input className='input' type='email' placeholder='email' value={email} onChange={(e)=>setemail(e.target.value)} /> 
         <input className='input' type='password' placeholder='password' value={password} onChange={(e)=>setpassword(e.target.value)} /> 
         <button className='button' onClick={handleClick} type='button'>Submit</button> 
     { runlogin&&<p className=' alert alert-success'> {datamsg} </p>}
+    { runerror&&<p className=' alert alert-error'> {datamsg} </p>}
            </div>
 
           
@@ -42,4 +53,4 @@ const [datamsg, setDatamsg] = useState('')
   )
 }
 
-export default Login
+export default Register
